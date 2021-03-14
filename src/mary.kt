1e267236f7daa98620d476b7ec2947cdf748fb3e
maryakinyi <owuorakinyimary@gmail.com>
2021-03-14 17:01:29 +0300

    kotlin assignment3

---
+++ src/mary.kt
@@ -0,0 +1,36 @@
+fun main (){
+var text="AkiraChix"
+    println(text[0])
+    println(text[2])
+    println(text[3])
+    var name="mary"
+    var age=21
+    println("my name is $name and I am $age years old .")
+    glitter()
+    that("mary")
+    that("Daina")
+
+}
+fun index(text:String){
+    var text="AkiraChix"
+    println(text)
+
+}
+fun self(name:String,age:Int){
+    var self="my name is $name and I am $age years old ."
+    println(self)
+}
+fun glitter(){
+var glitter="resource"
+    println(glitter.length)
+}
+fun that(name:String){
+    if (name=="mary"){
+            println("that's me!")
+            }
+    else{
+        println("i don't know who that is")
+    }
+
+
+}
